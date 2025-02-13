a8096aadbbfeb4f2dd051bd7491e9f830fdb4ae7
Matthew Packham <matthew_packham@outlook.com>
2025-02-13 17:28:52 +0000

    README.md File Updated

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # youtube-transcript-extractor
 A YouTube video transcript extractor. Prefers manual transcriptions over automatic speech recognition (ASR) if available 
+
+## How to Run
+
+To start the project, run:
+
+```bash
+npm run start
--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
 import { getYoutubeTranscript } from './yt-scraper.ts';
 import fs from 'fs/promises';
 
-async function main() {
-    const url = 'https://www.youtube.com/watch?v=nNRomzPyof0';
+async function main(url: string) {
     try {
         const videoId = url.match(/[?&]v=([^&]+)/)?.[1] || 'unknown';
         console.log(`Processing ${videoId}...`);
         
         const transcript = await getYoutubeTranscript(url);
-        await fs.writeFile(`../School _of_Life_Channel_Transcripts/transcript-${videoId}.txt`, transcript, 'utf-8');
+        await fs.writeFile(`../Transcripts/transcript-${videoId}.txt`, transcript, 'utf-8');
         console.log(`✅ Saved to transcript-${videoId}.txt`);
         
     } catch (error) {
         console.error('Error:', error instanceof Error ? error.message : error);
     }
 }
-
-main();
+const url = 'https://www.youtube.com/watch?v=qdBavW0x62k' 
+main(url);
